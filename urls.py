7be36f97cfd0acd92c9fd736f55aeb4dc847d043
RonitJ <50566982+RonitJ@users.noreply.github.com>
2019-11-12 18:24:54 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -11,24 +11,24 @@ app = Flask(__name__)
 #if __name__ == '__main__': 
   
 #    app.run()
-#tasks = [
-#    {
-#        'id': 1,
-#        'title': u'Buy groceries',
-#        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-#        'done': False
-#    },
-#    {
-#        'id': 2,
-#        'title': u'Learn Python',
-#        'description': u'Need to find a good Python tutorial on the web', 
-#        'done': False
-#    }
-#]
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
 
-#@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-#def get_tasks():
-#    return jsonify({'tasks': tasks})
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
